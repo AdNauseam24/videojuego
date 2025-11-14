43a5bd12de67ce2e637efa3d28428c9c04dc1ec1
Pascual Quiles <pascualquiles.contacto@gmail.com>
2025-11-14 10:52:16 +0100

    Añadidas colisiones

--- JuegoGranja/Assets/Scripts/Movimiento.cs
+++ JuegoGranja/Assets/Scripts/Movimiento.cs
@@ -7,6 +7,9 @@ public class Movimiento : MonoBehaviour
    //El punto al que queremos que el jugador se desplace en cada movimiento, 
    // con el fin de que siempre esté en los márgenes de una celda
     public Transform movePoint;
+
+    //La capa con la cual va a colisionar nuestro personaje
+    public LayerMask colisionar;
     
     void Start()
     {
@@ -20,18 +23,35 @@ public class Movimiento : MonoBehaviour
         //El jugador se mueve al movepoint según la velocidad
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, vMovimiento * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.1f)
+        //Para limitar el movimiento del movepoint, que solo se podrá volver a mover cuando el jugador esté a punto
+        //de alcanzarlo, evitando movimiento infinito
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.01f)
         {
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
             {
+                /*Antes de realizar el movimeinto comprobamos que el punto al que nos queremos mover no es de colisión,
+                Para ello dibujamos un círculo en el punto al que nos queremos desplazar y si está en la capa de colisiones no se 
+                realiza el movimiento
+                */
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, colisionar))
+                {
+
                 //movemos el movepoint por si el botón está presionado
                 movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+
+                }
             }
             if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
             {
+                 if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, colisionar))
+                {
+
                 //movemos el movepoint por si el botón está presionado
                 movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+
+                }
+
             }
         }
     }
